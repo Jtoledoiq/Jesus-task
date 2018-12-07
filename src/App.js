fd1e2be3b5bb08e9f8dacb1bfeac58150bb1b3e6
Jesús Toledo <43845286+Jtoledoiq@users.noreply.github.com>
2018-12-07 12:58:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,32 +8,88 @@ class App extends React.Component {
   constructor(props) {
       super(props);
       this.state = {value: ''};
+      this.changeColor = this.changeColor.bind(this);
+      this.state = {
+        isLoaded: false,
+        items1: ''
+    };
+    this.stateTwo = {
+      isLoaded: false,
+      items2: ''
+  };
 
-      this.getState = this.getState.bind(this);
+
+
     }
 
   myInput = React.createRef();
 
+  componentDidMount(){
 
-  getState (event)  {
+    fetch('http://www.colr.org/json/color/random')
+        .then(res=>res.json())
+        .then(json=>{
+          this.setState({
+            isLoaded: true,
+            items1:json,
+          })
+          //console.log(this.state.items.colors[0].hex)
+        //  let randomOne =this.state.items.colors[0].hex;
+      //  console.log(this.state)
+
+        })
+
+        fetch('http://www.colr.org/json/color/random')
+            .then(res=>res.json())
+            .then(json=>{
+              this.setState({
+                isLoaded: true,
+                items2:json,
+              })
+
+
+            })
+
+
+  }
+
+  getState = (event) => {
     this.setState({value: event.target.value});
     console.log(this.state.value);
+    //console.log(this.state.items.colors[0].hex)
+    //console.log(this.state.items2.colors[0].hex)
 
-  //1. Stop the form from submitting
-    //event.preventDefault();
-  // 2. get the text from that input
-  ///this.setState({value: event.target.value});
+
 }
 
 
 
+changeColor = (event) => {
+  const randomOne =`#${this.state.items1.colors[0].hex}`;
+  const randomTwo =`#${this.state.items2.colors[0].hex}`;
+  console.log(randomOne)
+  console.log(randomTwo)
+  this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+
+    if (this.state.isToggleOn) {this.setState({bColor:"black"})}
+    if (!this.state.isToggleOn) {
+      if (Math.random()>0.5) {this.setState({bColor:randomOne})}
+      else {this.setState({bColor:randomTwo})}
+    }
+
+
+}
+
+
   render() {
-    var jesus={color: 'blue'};
+
     return (
 
 
         <form className="store-selector" >
-        <div style={jesus}> {this.state.value}</div>
+        <div style={{backgroundColor:this.state.bColor}} onClick={this.changeColor} props="true"> {this.state.value}</div>
               <input type="text"
                 ref={this.myInput}
                 onChange={this.getState}
@@ -41,8 +97,7 @@ class App extends React.Component {
                 required
                 placeholder="Write something"
                 />
-              <button type="submit"> Click</button>
-       </form>
+        </form>
 
 
     );
